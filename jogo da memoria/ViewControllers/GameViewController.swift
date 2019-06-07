1ff9045991564eac2e57f8a80d481473e2963381
Mari Wronka <marielle.wronka@hotmail.com>
2019-06-07 02:26:52 -0300

    feat(new-game)

--- jogo da memoria/ViewControllers/GameViewController.swift
+++ jogo da memoria/ViewControllers/GameViewController.swift
@@ -18,23 +18,6 @@ class GameViewController: UIViewController {
     
     var cardTextProvider: CardTextProvider!
     
-    @IBAction func touchNewGame(_ sender: UIButton) {
-        for index in game.cards.indices {
-            let card = self.game.cards[index]
-            if card.isFaceUp == false { return }
-        }
-        
-        game = Game(numberOfPairOfCards: buttonsArray.count / 2)
-        flipCount = 0
-        
-        for index in game.cards.indices {
-            let button = self.buttonsArray[index]
-            button.setBackgroundImage(UIImage(named: "card_back.png"), for: .normal)
-            button.setTitle(" ", for: .normal)
-        }
-        
-    }
-    
     var flipCount = 0 { didSet { flipCountLabel.text = "Flips: \(flipCount)" } }
     
     lazy var game = Game(numberOfPairOfCards: buttonsArray.count / 2)
